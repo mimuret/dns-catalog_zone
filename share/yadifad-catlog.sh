7b04065ed3fb5a0d0d6404a956cdc6916260ccf3
Manabu Sonoda <mimuret@gmail.com>
2017-01-28 10:01:26 +0900

    update yadifa shell

--- share/yadifad-catlog.sh
+++ share/yadifad-catlog.sh
@@ -3,21 +3,28 @@
 # add yadifad.conf
 # >> include catlog.conf
 #
-if [ "$YADIFA_CONF_DIR" = "" ] ; then
-  NSD_DIR='/usr/local/etc'
+DNS_CATLOG_ZONE_DIR='/usr/local/dns-catlog_zone'
+YADIFA_CONF_DIR='/usr/local/etc'
+PID_FILE=/var/run/yadifad/yadifa.pid
+CONF=$YADIFA_CONF_DIR/catlog-zone.conf
+
+# write zonefile
+if [ -e "$PID_FILE" ] ; then
+  kill -USR1 `cat $PID_FILE`
 fi
-if [ "$CATZ" = "" ] ; then
-  CATZ='bundle exec catz'
-fi
-if [ "$YADIFA" = "" ] ; then
-  YADIFA='yadifa'
+# make config
+cd $DNS_CATLOG_ZONE_DIR
+bundle exec catz make $1 > $CONF.new 2>/dev/null
+
+if [ $? -eq 0 ] ; then
+    diff $CONF.new $CONF > /dev/null 2>&1
+    if [ $? -ne 0 ] ; then 
+      echo "reconfig"
+      mv $CONF.new $CONF
+      systemctl restart yadifad
+    fi
 fi
 
-conf=$YADIFA_CONF_DIR/catlog.conf
-new_conf=$YADIFA_CONF_DIR/catlog.conf.new
-
-$CATZ make $1 > new_conf
-if [ $? -eq 0 ] && `diff $new_conf $conf`  ; then
-    mv $new_conf $conf
-    $YADIFA cfgreload
+if [ -e $CONF.new ] ; then
+  rm $CONF.new
 fi
